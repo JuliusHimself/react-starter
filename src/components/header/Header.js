770c1dfd647bc1bc1a4650c30b02ae01c40886cc
julius <julius@gyft.com>
2019-10-30 14:30:09 -0700

    stetup scss and styles file structure

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../assets/img/logo.svg';
+
+
+export default function Header () {
+  return (
+    <div className='header'>
+        <img src={logo} className="App-logo" alt="logo"/>
+    </div>
+  )
+}
--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-function notFound() {
+export default function notFound() {
     return(
       <div>page not found</div>
     )
 }
-
-export default notFound;
